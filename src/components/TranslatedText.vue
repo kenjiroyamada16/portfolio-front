02fa5a94c4eaf70ce25a9e2686fc2728e969ca88
Nicolas Yamada <nickminevilil61@gmail.com>
2025-05-18 23:26:06 -0400

    [UPDATE] translate api text and layout adjustments

---
+++ src/components/TranslatedText.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>{{ getTranslatedText }}</span>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue';
+  import { useI18n } from 'vue-i18n';
+
+  const { locale } = useI18n();
+
+  const props = defineProps<{
+    translatedText: Record<string, string>;
+  }>();
+
+  const getTranslatedText = computed<string>(() => {
+    return props.translatedText[locale.value];
+  });
+</script>
